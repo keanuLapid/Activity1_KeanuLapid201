453ed276f7925af0f92f2911069271cde808cb76
DrexMac <Dmacaspac@csmbataan.ph.education>
2025-03-12 18:09:50 +0800

    Add project files.

---
+++ keanulapid.Models/LoginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keanulapid.Models
+{
+    internal class LoginInfo
+
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string ? LoggingType { get; set; }
+        public DateTime ? CreatedAt {  get; set; }
+        public User ? User { get; set; }
+
+
+    }
+}
